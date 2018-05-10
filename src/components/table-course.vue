6b42e86ad01b2095c7075d31bda8442dbcc7c6a4
廖星 <xing.liao@wutnews.net>
2018-05-10 15:30:25 +0800

    支持查看教师详细信息

--- src/components/table-course.vue
+++ src/components/table-course.vue
@@ -1,7 +1,8 @@
 <template>
     <div>
         <p class="course-title">授课教师</p>
-        <p class="course-content">{{course.teacher.name ? course.teacher.name : '暂无'}}</p>
+        <p class="course-content" v-if="course.id > 0">{{course.teacher.name ? course.teacher.name : '暂无'}}</p>
+        <p class="course-content course-link" v-else onclick="token.loadUrl('https://web.wutnews.net/table/index/teacher?sno=' + this.getAttribute('sno').replace(/^0+/g, ''))" :sno="course.teacher.sno">{{course.teacher.name}}</p>
         <p class="course-divide">&nbsp;</p>
         <p class="course-title">授课时间</p>
         <p class="course-content">{{timeStr}}</p>
@@ -29,25 +30,33 @@
 </script>
 
 <style scoped>
-    .course-title {
-        font-size: 0.85rem;
-        color: #bbb;
-        margin: 0.5rem 0;
-    }
+  .course-title {
+    font-size: 0.85rem;
+    color: #bbb;
+    margin: 0.5rem 0;
+  }
 
-    .course-content, .course-footer {
-        font-size: 1.1rem;
-        color: #000;
-        margin: 0.5rem 0;
-    }
+  .course-content, .course-footer {
+    font-size: 1.1rem;
+    color: #000;
+    margin: 0.5rem 0;
+  }
 
-    .course-footer {
-        text-align: center;
-        margin: 2rem 0.5rem -0.3rem;
-    }
+  .course-footer {
+    text-align: center;
+    margin: 2rem 0.5rem -0.3rem;
+  }
 
-    .course-divide {
-        margin: 0;
-        line-height: 0.3rem !important;
-    }
+  .course-divide {
+    margin: 0;
+    line-height: 0.3rem !important;
+  }
+
+  .course-link {
+    cursor: pointer;
+    background-image: url("data:image/svg+xml;charset=utf-8,%3Csvg%20viewBox%3D'0%200%2060%20120'%20xmlns%3D'http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg'%3E%3Cpath%20d%3D'm60%2061.5-38.25%2038.25-9.75-9.75%2029.25-28.5-29.25-28.5%209.75-9.75z'%20fill%3D'%23c7c7cc'%2F%3E%3C%2Fsvg%3E");
+    background-size: 10px 20px;
+    background-repeat: no-repeat;
+    background-position: 100% center;
+  }
 </style>
